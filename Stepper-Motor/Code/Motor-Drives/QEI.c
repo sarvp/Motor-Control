cb9beb6cfd9f5d6821e25fdef267cea7e0b72c1d
sarvp <41444785+sarvp@users.noreply.github.com>
2023-10-15 08:29:34 +0530

    Add files via upload

---
+++ Stepper-Motor/Code/Motor-Drives/QEI.c
@@ -0,0 +1,94 @@
+/*
+ * QEI.c
+ *
+ *  Created on: 19-Aug-2023
+ *      Author: Sarvjit
+ */
+
+#include "QEI.h"
+
+#ifndef UART_H_
+#include"UART.h"
+#endif
+
+void QEI_0_INIT(void)
+{
+    SYSCTL_RCGC1_R |= SYSCTL_RCGC1_QEI0 ;
+    SYSCTL_RCGCQEI_R |= SYSCTL_RCGCQEI_R0 ;
+
+    while((SYSCTL_PRQEI_R & SYSCTL_PRQEI_R0) == 0){}; // Wait for clock to stabilize
+
+    SYSCTL_RCGC2_R |= SYSCTL_RCGC2_GPIOD ;
+    SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R3 ;
+
+    while((SYSCTL_PRGPIO_R & SYSCTL_PRGPIO_R3) == 0){}; // Wait for clock to stabilize
+
+    GPIO_PORTF_DIR_R |= 0x00000000 ;    /*  GPIO Direction | 0 -> INPUT | 1 -> OUTPUT */
+
+    GPIO_PORTF_DIR_R &= ~ 0x00000013 ;
+
+    GPIO_PORTF_CR_R |= 0xFFFFFFFF ;     /* 1 -> The corresponding GPIOAFSEL, GPIOPUR, GPIOPDR, or GPIODEN bits can be written */
+
+    GPIO_PORTF_AFSEL_R &= ~ 0xFFFFFF00 ;  /* Disable alternate function on PORT */
+
+    GPIO_PORTF_AFSEL_R |= 0x00000013 ;
+
+    GPIO_PORTF_AMSEL_R &= ~ 0xFFFFFFFF ;  /* Disable analog function on PORT */
+
+    GPIO_PORTF_PCTL_R &= ~ 0xFFFFFF00 ; /* Regular digital function */
+
+    GPIO_PORTF_PCTL_R |= 0x00060066 ;
+
+    QEI0_CTL_R |= 0x00000038 ;
+
+    QEI0_MAXPOS_R = 0x00001FFF ;
+
+    QEI0_LOAD_R = 160000 - 1 ;
+
+    QEI0_CTL_R |= 0x00000001 ;
+
+    QEI0_MAXPOS_R = 0x00001FFF ;
+
+    QEI0_POS_R = 0x00 ;
+
+    /* Interrupt */
+
+    EnableInterrupts() ;             /* Enable global Interrupt flag (I) */
+
+    NVIC_PRI3_R |= (NVIC_PRI3_R & 0xFFFF1FFF) | 0x0000C000 ; /*  priority 6 */
+
+    NVIC_EN0_R |= 0x00002000 ;        /*  Enable interrupt 13 in NVIC */
+
+    QEI0_INTEN_R |= 0x00000005 ;
+
+}
+
+char Q_str[100] ;
+
+long rpm = 0;
+
+void QEI_0_Handler(void)
+{
+    if( QEI0_RIS_R & 0x00000004 )
+    {
+        QEI0_ISC_R |= 0x00000004 ;
+
+//        uart0_send_str("DIR\n");
+//        DIR
+    }
+
+    if( QEI0_RIS_R & 0x00000001 )
+    {
+        QEI0_ISC_R |= 0x00000001 ;
+
+//        rpm = (10000 * ( (QEI0_SPEED_R & 0xFFFF0000) >> 16 ) * 60);
+//
+//        uart0_send_str("I\n");
+//        sprintf(Q_str,"RPM = %ld\n", rpm);
+//        uart0_send_str(Q_str);
+//        index
+    }
+}
+
+
+
---
+++ Stepper-Motor/Code/Motor-Drives/main.c
@@ -0,0 +1,295 @@
+/*
+ * L298N H Bridge Driver using PWM
+ * Sarvjit Ajit Patil 20862
+ * Date : 08/08/2023
+ */
+
+
+
+/*
+ * main.c
+ */
+#include <stdio.h>
+
+#include <stdint.h>
+
+#include <string.h>
+
+#include <stdlib.h>
+
+#include "D:/ti/TivaWare_C_Series-2.2.0.295/TivaWare_C_Series-2.2.0.295/inc/tm4c123gh6pm.h"
+
+#include"SysTick.h"
+
+#include"PWM.h"
+
+#ifndef ADC_H_
+#include"ADC.h"
+#endif
+
+#include"GPIO.h"
+
+#ifndef UART_H_
+#include"UART.h"
+#endif
+
+#ifndef QEI_H_
+#include "QEI.h"
+#endif
+
+// #include"LCD.h"
+
+// #include"utils.h"
+
+#ifndef TIMER_H_
+#include"timer.h"
+#endif
+
+#ifndef STEPPER_H_
+#include"stepper.h"
+#endif
+
+//#define DEBUG
+
+// #include"ultrasonic.h"
+
+void delayMs(int n);
+
+#define PWM_FRE 20000
+
+extern char RX1_BUF[200], RX0_BUF[200], RX1_CMD[200] ;
+
+extern int rx0_command_flag ;
+
+extern unsigned long ms_counter, h_us_cnt ;
+
+extern int dir, done_flag ;
+
+volatile int ADC_Voltage, ADC_Current ;
+
+extern int ADC_Sel_cnt ;
+
+float pos = 0 ;
+long cur_cnt = 0 ;
+char str[100] ;
+
+//const int half_step_seq[8] = {0x00000080, 0x000000C0, 0x00000040, 0x00000060, 0x00000020, 0x00000030, 0x00000010, 0x00000090} ;
+const int half_step_seq[8] = {0x00000080, 0x000000A0, 0x00000020, 0x00000060, 0x00000040, 0x00000050, 0x00000010, 0x00000090} ;
+const int full_step_seq[4] = {0xA0, 0x60, 0x50, 0x90 } ;
+
+extern unsigned long h_us_stepper_counter ;
+
+extern void PWM_Start(void);
+
+/* Set Phase currents (in Amps) */
+
+#define SET_CURR_A 1.5
+
+#define SET_CURR_B 1.5
+
+/* Set RPM */
+int RPM = 150 ;
+
+float set_Curr_A_ADC = ((SET_CURR_A * 4095) / 3.3);
+
+float set_Curr_B_ADC = ((SET_CURR_B * 4095) / 3.3);
+
+int I_Set = 0 ;
+
+volatile int sequence = 0;
+
+float Voltage, Current, R_RPM, Position;
+
+/* Set sampling time Ts for PWM */
+#define Ts 500
+
+int main(void)
+{
+    EnableInterrupts() ;
+
+    Systick_init();
+//    GPIOA_INIT();
+    GPIOB_INIT();
+//    GPIOC_INIT();
+//    GPIOD_INIT();
+//    GPIOE_INIT();
+    GPIOF_INIT();
+
+//    TIMER0_INIT();
+
+    uart0_init();
+//    uart1_init();
+
+    uart0_send_str("Welcome to UART0\n");
+    uart0_send_str("This is for debugging.\n");
+
+    QEI_0_INIT();
+
+    /* Init PWM for micro-stepping */
+
+    //    float set_duty = 0.5 ;
+//    PWM_0_SYNC() ;
+
+    M0PWM2_init(Ts);
+    M0PWM3_init(Ts);       /* PWM is used as it triggers the ADC */
+    M0PWM4_init(Ts);
+    M0PWM5_init(Ts);
+
+    PWM_Start();
+
+//    M0PWM5_init(PWM_FRE);
+//    M0PWM4_set(set_duty);
+//    M0PWM2_set(set_duty);
+    PWM_0_SYNC();
+//
+//    update_PWM(45,2);
+
+
+//    M0PWM5_set(set_duty);
+
+//    ADC_0_SS_3_INIT() ;
+
+//    ADC_0_SS_0_INIT();
+
+//    ADC_0_SS_1_INIT();
+
+//    ADC_1_SS_1_INIT();
+
+    int DIR = 1 ;
+
+    for( int i = 0; i < 360 ; i++)
+    {
+        update_PWM(i, 2) ;
+    }
+
+    while(1)
+    {
+        if ( rx0_command_flag )
+        {
+            I_Set = atoi(RX0_BUF);
+
+            update_PWM(I_Set, 2) ;
+            RPM = I_Set ;
+
+//            delayMs(20) ;
+//
+            sprintf(str,"Set Position = %d.%d\n", I_Set, 0);
+            uart0_send_str(str);
+
+            for(int i = 0; i< 318000; i++);
+
+            Position = ( QEI0_POS_R * 360 ) / ( 8192 ) ;
+            Position = Position * 1000 ;
+
+            int P_i = (int)(Position) / 1000;
+            int P_f = (int) (Position) % 1000 ;
+            sprintf(str,"Read Position = %d.%d\n", P_i, P_f);
+            uart0_send_str(str);
+//
+//            float rpm = (6400 * ( (QEI0_SPEED_R) ) * 60) / (2048 * 4);
+//            rpm = rpm * 1000 ;
+//
+//            int R_i = (int)(rpm) / 1000;
+//            int R_f = (int) (rpm) % 1000 ;
+//            sprintf(str,"RPM = %d.%d\n", R_i, R_f);
+//            uart0_send_str(str);
+
+//            int set_Curr_ADC = ((I_Set * 4.095) / 3.3);
+//
+//            ADC0_DCCMP1_R   = ((int)set_Curr_ADC<<16);    // Set high region threshold for DC1
+//            ADC1_DCCMP2_R   = ((int)set_Curr_ADC<<16);    // Set high region threshold for DC2
+
+//            sprintf(str,"Set Current = %d mA\n", I_Set);
+//            uart0_send_str(str);
+            rx0_command_flag = 0 ;
+
+        }
+
+        /* Full Step revolution */
+        for(int i = 0; i < 360; i++)
+        {
+            update_PWM(i,2);
+//            delayMs(2) ;
+
+//            sprintf(str,"Set Position = %d.%d\n", i, 0);
+//            uart0_send_str(str);
+//            Position = ( QEI0_POS_R * 360 ) / ( 8192 ) ;
+//            Position = Position * 1000 ;
+//
+//            int P_i = (int)(Position) / 1000;
+//            int P_f = (int) (Position) % 1000 ;
+//            sprintf(str,"Read Position = %ld.%d\n", P_i*200, P_f);
+//            uart0_send_str(str);
+//        }
+//        if ( h_us_stepper_counter > (15000/(RPM)) )
+//        {
+//            h_us_stepper_counter = 0 ;
+//
+//            sequence = sequence + (5*DIR);
+//            update_PWM(sequence,2);
+//
+//            if( sequence > 359 )
+//            {
+//                    sequence = 0 ;
+//            }
+//            else if( sequence < 0 )
+//            {
+//                sequence = 359 ;
+//            }
+
+//            float rpm = (6400 * ( (QEI0_SPEED_R) ) * 60) / (2048 * 4);
+//            rpm = rpm * 1000 ;
+//
+//            int R_i = (int)(rpm) / 1000;
+//            int R_f = (int) (rpm) % 1000 ;
+//            sprintf(str,"RPM = %d.%d\n", R_i, R_f);
+//            uart0_send_str(str);
+        }
+
+        /* Half Step/ Full Step */
+//        if ( h_us_stepper_counter > (15000/RPM) )
+//        {
+//            h_us_stepper_counter = 0 ;
+//
+//            sequence = sequence + DIR;
+//            if( sequence > 7 )
+//            {
+//                sequence = 0 ;
+//            }
+//            else if( sequence < 0 )
+//            {
+//                sequence = 7 ;
+//            }
+//
+//        }
+//
+//        GPIO_PORTB_DATA_R = half_step_seq[sequence] ;
+
+        /* Display Parameters */
+//        if(h_us_cnt > 10000)
+//        {
+//            h_us_cnt = 0 ;
+//            float rpm = (100 * ( (QEI0_SPEED_R) ) * 60) / (2048 * 4);
+////            rpm = rpm * 1000 ;
+//
+////            int R_i = (int)(rpm) / 1000;
+////            int R_f = (int) (rpm) % 1000 ;
+////            sprintf(str,"RPM = %d.%d\n", R_i, R_f);
+//            sprintf(str,"RPM = %d\n", (int)rpm);
+////            sprintf(str,"RPM = %ld\n", QEI0_SPEED_R);
+//            uart0_send_str(str);
+//        }
+        WaitForInterrupt();
+
+    }
+}
+
+void delayMs( int n )
+{
+    int i, j ;
+
+    for(i = 0; i < n ; i ++)
+    {
+        for(j = 0; j < 2000; j++);
+    }
+}
